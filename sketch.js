7a89198f7e0c7b69405cbaf4501994264073dabc
IanchuO <99927149+IanchuO@users.noreply.github.com>
2022-02-22 12:10:36 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ var seaImg,shipImg;
 function preload(){
   //descomentar el código para agregar la animación del barco 
 
-  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
   //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
